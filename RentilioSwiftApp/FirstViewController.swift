1e7c3f2ff1fc0f6759d051e907461c7556de97d6
Rafles29 <rwozniak@intelgraf.com.pl>
2020-04-24 15:05:58 +0200

    Initial Commit

---
+++ RentilioSwiftApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RentilioSwiftApp
+//
+//  Created by Rafal Wozniak on 24/04/2020.
+//  Copyright © 2020 Rafal Wozniak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RentilioSwiftApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RentilioSwiftApp
+//
+//  Created by Rafal Wozniak on 24/04/2020.
+//  Copyright © 2020 Rafal Wozniak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
